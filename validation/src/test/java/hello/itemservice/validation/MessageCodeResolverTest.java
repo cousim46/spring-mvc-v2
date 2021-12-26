ec1e686c0cb0fd199d7b05dfd61584a5ac0f22fc
cousim46 <wjdghldns24@naver.com>
2021-12-27 04:09:03 +0900

    BindingResult - typeMismatch

---
+++ validation/src/test/java/hello/itemservice/validation/MessageCodeResolverTest.java
@@ -0,0 +1,31 @@
+package hello.itemservice.validation;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.validation.DefaultMessageCodesResolver;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.validation.ObjectError;
+
+public class MessageCodeResolverTest {
+
+    MessageCodesResolver codesResolver = new DefaultMessageCodesResolver();
+
+    @Test
+    void messageCodeResolverObject() {
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item");
+        for (String ms : messageCodes) {
+            System.out.println("ms = " + ms);
+        }
+        Assertions.assertThat(messageCodes).containsExactly("required.item", "required");
+    }
+
+    @Test
+    void messageCodesResolverField() {
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item", "itemName", String.class);
+        for (String messageCode : messageCodes) {
+            System.out.println("messageCode = " + messageCode);
+            
+        }
+
+    }
+}
